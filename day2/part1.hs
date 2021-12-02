633d7ac17ff7653077599a85f5836224d997b679
Steve Wolligandt <steve@isg.cs.ovgu.de>
2021-12-02 14:57:17 +0100

    day2 part1 working

--- day2/part1.hs
+++ day2/part1.hs
@@ -2,16 +2,46 @@ module Main where
 --------------------------------------------------------------------------------
 import System.IO
 --------------------------------------------------------------------------------
-readInput = do
-  handle  <- openFile "input.txt" ReadMode
-  content <- hGetContents handle
-  let ws = (words content)
-  hClose handle
-  return ws
+data DiveDirection = Forward | Up | Down | Unknown deriving Show
+--------------------------------------------------------------------------------
+parseDirection :: String -> DiveDirection
+parseDirection str 
+  | str == "forward" = Forward
+  | str == "up"      = Up
+  | str == "down"    = Down
+  | otherwise        = Unknown
+--------------------------------------------------------------------------------
+parseInt :: String -> Int
+parseInt str = read str
+--------------------------------------------------------------------------------
+parseDiveCommand :: String -> (DiveDirection, Int)
+parseDiveCommand line = 
+  (dir, num)
+  where ws  = words line
+        dir = parseDirection (ws!!0)
+        num = parseInt       (ws!!1)
+--------------------------------------------------------------------------------
+parseDiveCommands :: String -> [(DiveDirection, Int)]
+parseDiveCommands cmdsString = 
+  cmds
+  where ls   = lines cmdsString
+        cmds = map parseDiveCommand ls
+--------------------------------------------------------------------------------
+dive :: (Int,Int) -> [(DiveDirection, Int)] -> (Int, Int)
+dive (x,y) ((Forward, i):rest) = dive (x + i,y) rest 
+dive (x,y) ((Up     , i):rest) = dive (x,y - i) rest 
+dive (x,y) ((Down   , i):rest) = dive (x,y + i) rest 
+dive (x,y) ((Unknown, i):rest) = dive (x,y) rest 
+dive pos [] = pos
 --------------------------------------------------------------------------------
 main = do
-  input <- readInput
-  print (head input)
-  --print (length content)
-  --where
-  --  content = readInput "input.txt"
+  handle  <- openFile "input.txt" ReadMode
+  content <- hGetContents handle
+  let startPos = (0,0)
+      diveCmds = parseDiveCommands content
+      endPos   = dive startPos diveCmds
+  print (head diveCmds)
+  print (length diveCmds)
+  print endPos
+  print ((fst endPos) * (snd endPos))
+  hClose handle
