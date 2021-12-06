b2e39b7ced6eb9321eb5f1056f28ed067e04bbbd
Steve Wolligandt <stevewolligandt@gmail.com>
2021-12-06 23:23:41 +0100

    day4 working

---
+++ day4.hs
@@ -0,0 +1,12 @@
+module Main where
+import AdventOfCode
+import AdventOfCode.Day4
+--import AdventOfCode.Day3.Part1
+--import AdventOfCode.Day3.Part2
+
+main = do
+  --operateOnInput "test.day4.txt" test1
+  --putStrLn ""
+  operateOnInput "input.day4.txt" part1
+  --putStrLn ""
+  --operateOnInput "input.day4.txt" part2
